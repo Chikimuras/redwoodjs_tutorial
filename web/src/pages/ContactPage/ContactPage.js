e62ed29e52dffb31f4a18f25080d6aff9ed7648a
Chikimuras <alexandre.m.velia@gmail.com>
2022-05-09 23:47:20 +0200

    Saving Data in Redwood + Chapter 3 : Done

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,4 +1,4 @@
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags, useMutation } from '@redwoodjs/web'
 import {
   FieldError,
   Form,
@@ -6,18 +6,45 @@ import {
   Label,
   Submit,
   TextAreaField,
+  FormError,
+  useForm,
 } from '@redwoodjs/forms'
+import { toast, Toaster } from '@redwoodjs/web/dist/toast'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 const ContactPage = () => {
+  const formMethods = useForm()
+
+  const [createContact, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success('Thank you for submission')
+      formMethods.reset()
+    },
+  })
+
   const onSubmit = (data) => {
-    alert(JSON.stringify(data))
+    createContact({ variables: { input: data } })
   }
 
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
       <h1>Contact Us</h1>
-      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+      <Toaster />
+      <Form
+        onSubmit={onSubmit}
+        config={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        <FormError error={error} wrapperClassName="form-error" />
         <div className="field">
           <Label name="name" errorClassName="error">
             Name
@@ -37,10 +64,6 @@ const ContactPage = () => {
             name="email"
             validation={{
               required: true,
-              pattern: {
-                value: /^[^@]+@[^.]+\..+$/,
-                message: 'Please enter a valid email address',
-              },
             }}
             errorClassName="error"
           />
@@ -57,7 +80,7 @@ const ContactPage = () => {
           />
           <FieldError name="message" className="error" />
         </div>
-        <Submit>Save</Submit>
+        <Submit disabled={loading}>Save</Submit>
       </Form>
     </>
   )
